876ef7e4f41505ab4cebf3552628c93617361491
Nguyen Gia Huy <nguyengiahuy27201@gmail.com>
2021-11-30 19:01:15 +0700

    config the database hosting

--- components/product/productController.js
+++ components/product/productController.js
@@ -7,7 +7,22 @@ module.exports = {
         try {
             //get request params
             const page = (!isNaN(req.query.page) && req.query.page > 0) ? Number(req.query.page) : 1;
-            const search = req.query.search;
+
+            //get product list, category and page count
+            const products = await productService.list(page - 1);
+            const category = await productService.category();
+            const maxPage = Math.floor((products.count.length - 1) / 8) + 1;
+
+            res.render('product/products', { title: 'Products', products: products.rows, category, currentPage: page, maxPage });
+        } catch (err) {
+            console.log(err.message);
+        }
+    },
+    search: async(req, res) => {
+        try {
+            //get request params
+            const page = (!isNaN(req.query.page) && req.query.page > 0) ? Number(req.query.page) : 1;
+            const search = req.query.keyword;
 
             //get product list, category and page count
             const products = search ? await productService.findName(search, page - 1) : await productService.list(page - 1);
@@ -19,7 +34,6 @@ module.exports = {
             console.log(err.message);
         }
     },
-
     addProductPage: async(req, res) => {
         try {
             const category = await productService.category();
--- hbsHelpers.js
+++ hbsHelpers.js
@@ -4,24 +4,25 @@ const helpers = (hbs) => {
 
     // Huy sẽ sửa cái này sau
     hbs.registerHelper('page', (currentPage, maxPage, search) => {
-        const searchTerm = typeof search !== 'undefined' ? `search=${search}&` : '';
-        let item = "<li class=\"page-item d-none d-sm-block " + (currentPage == 1 ? "disabled" : "") + "\">" + 
-                        "<a class=\"page-link\" href=\"?" + searchTerm + "page=" + (currentPage - 1) + "\" aria-label=\"Previous\">" +
-                            " <i class=\"bi bi-caret-left-fill\"></i>" +
-                        "</a>"+
-                    "</li>\n";
+        const searchTerm = typeof search !== 'undefined' ? `keyword=${search}&` : '';
+
+        let item = `<li class="page-item d-none d-sm-block ${currentPage == 1 ? "disabled" : ""}">
+                        <a class="page-link" href="?${searchTerm}page=${currentPage - 1}" aria-label="Previous">
+                            <i class="bi bi-caret-left-fill"></i>
+                        </a>
+                    </li>`;
         for (let i = 1; i <= maxPage; i++) {
-            item += "<li class=\"page-item " + (i == currentPage ? "active" : "") + "\">" + 
-                        "<a class=\"page-link\" href=\"?" + searchTerm + "page=" + i + "\" aria-label=\"" + i + "\">" +
-                            "<span>" + i + "</span>" +
-                        "</a>"+
-                    "</li>\n";
+            item += `<li class="page-item ${i == currentPage ? "active" : ""}">
+                        <a class="page-link" href="?${searchTerm}page=${i}" aria-label="${i}">
+                            <span>${i}</span>
+                        </a>
+                    </li>`;
         }
-        item += "<li class=\"page-item d-none d-sm-block " + (currentPage == maxPage ? "disabled" : "") + "\">" + 
-                        "<a class=\"page-link\" href=\"?" + searchTerm + "page=" + (currentPage + 1) + "\" aria-label=\"Previous\">" +
-                            " <i class=\"bi bi-caret-right-fill\"></i>" +
-                        "</a>"+
-                    "</li>\n";
+        item += `<li class="page-item d-none d-sm-block ${currentPage == maxPage ? "disabled" : ""}">
+                        <a class="page-link" href="?${searchTerm}page=${currentPage + 1}" aria-label="Next">
+                            <i class="bi bi-caret-right-fill"></i>
+                        </a>
+                    </li>`;
         return item;
     });
 }
