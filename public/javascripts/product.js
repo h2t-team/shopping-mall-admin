ab635ce213e6898bdf30305e7bfd31832139ddbb
Nguyen Gia Huy <nguyengiahuy27201@gmail.com>
2021-12-24 02:14:38 +0700

    adding product validation

--- public/javascripts/product.js
+++ public/javascripts/product.js
@@ -1,3 +1,19 @@
+const error = msg => `<div class="alert alert-danger d-flex align-items-center" role="alert">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-exclamation-triangle-fill flex-shrink-0 me-2" viewBox="0 0 16 16" role="img" aria-label="Warning:">
+                            <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+                        </svg>
+                            <div>
+                                ${msg}
+                            </div>
+                        </div>`
+const success = () => `<div class="alert alert-success d-flex align-items-center" role="alert">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-check-circle-fill flex-shrink-0 me-2" viewBox="0 0 16 16" role="img" aria-label="Success:">
+                        <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/>
+                            <div>
+                                Update account successfully!
+                            </div>
+                        </div>`                 
+
 $(document).ready(() => {
     // add product previews
     $('#photo').on('change', e => {
@@ -8,10 +24,8 @@ $(document).ready(() => {
         else {
             $('.product-img-container').remove();
             const { files } = e.target;
-            console.log(files)
             if (files.length > 0) {
                 for (let i = 0; i < files.length; i++) {
-                    console.log(files[i]);
                     const src = URL.createObjectURL(files[i]);
                     const imgContainer = `<div class="me-3 mb-3 product-img-container">
                                             <div>
@@ -29,17 +43,62 @@ $(document).ready(() => {
         const name  = $(this).data('name');
         const id = $(this).data('id');
         const category = $(this).data('category');
-        
+
         $('.modal-body').text(`Are you sure to want to remove ${name.toLowerCase()}?`);
         $('.confirm-btn').click(() => {
             removeProduct(id, category);
         })
     });
+});
 
-    // validate and submit form
-    $('#pcreate-btn').on('click', e => {
+const validateProduct = {
+    rules: {
+        "pname": {
+            required: true,
+        },
+        "pprice": {
+            required: true,
+            min: 1000,
+        },
+    },
+    messages: {
+        "pname": {
+            required: "Please enter a product name",
+        },
+        "pprice": {
+            required: "Please enter the price",
+            min: "Price must be at least 1000đ",
+        },
+    },
+    errorPlacement: (error, element) => {
+        isValid = false;
+        element.css('background-color', '#ffdddd');
+        error.css('color', 'red');
+        error.css('margin-top', '10px');
+        error.insertAfter(element);
+    },
+    unhighlight: element => {
+        isValid = true;
+        $(element).css('background-color', 'var(--mint)');
+    },
+}
+
+// validate and submit add product form
+$('#add-product-form').validate({
+    ...validateProduct,
+    submitHandler: function(form, event) {
+        event.preventDefault();
         handleAddProduct();
-    })
+    }
+})
+
+// validate and submit edit product form
+$('#update-product-form').validate({
+    ...validateProduct,
+    submitHandler: function(form, event) {
+        event.preventDefault();
+        submitUpdateProductForm();
+    }
 })
 
 var path = window.location.pathname;
@@ -78,7 +137,6 @@ function handleAddProduct() {
         $('#error-upload').append('Please choose an image!');
         return;
     }
-
     const form = document.getElementById("add-product-form");
     const table = document.getElementById("table-quantities");
     for (let i = 1; i < table.rows.length; i++) {
@@ -92,8 +150,6 @@ function handleAddProduct() {
     }
     form.submit();
 }
-
-
 
 // remove a product
 function removeProduct(id, category_id) {
@@ -109,23 +165,13 @@ function removeProduct(id, category_id) {
     .fail(err => {
         $("#errorMessage").empty();
         const msg = err.responseJSON.message;
-        $("#errorMessage").append(`
-            <div class="alert alert-danger d-flex align-items-center" role="alert">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-exclamation-triangle-fill flex-shrink-0 me-2" viewBox="0 0 16 16" role="img" aria-label="Warning:">
-                <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
-            </svg>
-                <div>
-                    ${msg}
-                </div>
-            </div>`
-        );
+        $("#errorMessage").append(error(msg));
     })
 }
 
 if (path.match('/products/updateproduct/')) {
     window.onload = function() {
         changePQuantityValue();
-
     }
     document.getElementById('psize').onchange = function() {
             changePQuantityValue();
@@ -201,12 +247,11 @@ function submitUpdateProductForm() {
         data: JSON.stringify(data), // data to submit
     }).done((res) => {
         $("#errorMessage").empty();
-        console.log("SUCCESS respones", res);
         window.location.href = "/products";
     }).fail((res) => {
         $("#errorMessage").empty();
         const msg = res.responseJSON.message;
-        $("#errorMessage").append(`<div class="alert alert-danger" role="alert">${msg}</div>`);
+        $("#errorMessage").append(error(msg));
     });
 
 }
