f44b8b10213442ed44a7d596062c38996c9a4ba7
Nguyen Gia Huy <nguyengiahuy27201@gmail.com>
2022-01-17 12:14:34 +0700

    fix

--- models/index.js
+++ models/index.js
@@ -1,23 +1,7 @@
 const Sequelize = require('sequelize');
 const initModels = require("./init-models");
 
-// const sequelize = new Sequelize(process.env.CLEARDB_DATABASE_URL, {
-//     timezone: '+07:00',
-//     dialectOptions: {
-//         useUTC: false, //for reading from database
-//         dateStrings: true,
-//         typeCast: function(field, next) { // for reading from database
-//             if (field.type === 'DATETIME') {
-//                 return field.string()
-//             }
-//             return next()
-//         },
-//     }
-// });
-
-const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
-    host: process.env.DB_HOST,
-    dialect: 'mysql',
+const sequelize = new Sequelize(process.env.CLEARDB_DATABASE_URL, {
     timezone: '+07:00',
     dialectOptions: {
         useUTC: false, //for reading from database
@@ -30,6 +14,22 @@ const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USERNAME
         },
     }
 });
+
+// const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+//     host: process.env.DB_HOST,
+//     dialect: 'mysql',
+//     timezone: '+07:00',
+//     dialectOptions: {
+//         useUTC: false, //for reading from database
+//         dateStrings: true,
+//         typeCast: function(field, next) { // for reading from database
+//             if (field.type === 'DATETIME') {
+//                 return field.string()
+//             }
+//             return next()
+//         },
+//     }
+// });
 
 module.exports = {
     sequelize,
